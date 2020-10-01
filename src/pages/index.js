a0416f292d7ac999e510fe41a7c7faf3f408a0f6
Kevin Hu <hukxchen+git@gmail.com>
2020-10-01 16:22:46 -0400

    feat: use Link from gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
-
+import {Link} from "gatsby"
 export default function Home() {
-  return <div>Hello Erica!</div>
+  return (
+    <div>
+      <h1>Hello Erica!</h1>
+      <Link to="/about">About</Link>
+    </div>
+  )
 }

888c78f9e315bb9a3c1434e83d7aae2deed2b717
Kevin Hu <hukxchen+git@gmail.com>
2020-10-02 11:53:27 -0400

    feat: use the gatsby-transformer-remark
    This makes markdown files available in the graphQL data layer for gatsby to query with an html field that is the markdown transformed to HTML

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,20 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [{
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `src`,
-      path: `${__dirname}/src/`
-    }
-  },
-  {
-    resolve: `gatsby-plugin-typography`,
-    options: {
-      pathToConfigModule: `src/utils/typography`
-    }
-  }],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }

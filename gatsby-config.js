0c9faa5083580ce1c98316d41fb7dfc4cf6bef6c
Kevin Hu <hukxchen+git@gmail.com>
2020-10-02 14:59:54 -0400

    feat: PWA service worker requirement met
    all requirements met except for use of HTTPS redirect and a few insecure requests for fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,24 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `hukev`,
+        background_color: "#fdf6e3",
+        theme_color: "#fdf6e3",
         short_name: `hukev`,
         start_url: `/`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // must be 512x512px - path relative to site root
+        icons: [
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: `maskable`,
+          },
+        ],
       },
     },
+    `gatsby-plugin-offline`,
   ],
 }

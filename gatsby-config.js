e0e33a77302fbfd189e7e170b7dd08d167f9352a
Kevin Hu <hukxchen+git@gmail.com>
2020-10-02 10:49:09 -0400

    feat: now Gatsby data layer is populated with files from the filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@
 module.exports = {
   /* Your site config here */
   plugins: [{
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`
+    }
+  },
+  {
     resolve: `gatsby-plugin-typography`,
     options: {
       pathToConfigModule: `src/utils/typography`

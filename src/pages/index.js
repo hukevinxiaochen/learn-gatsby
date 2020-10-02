3096d5e16b880941a23e9fb02264a4ffc2411024
Kevin Hu <hukxchen+git@gmail.com>
2020-10-02 08:12:01 -0400

    style: experiment with gatsby-browser.js and component-scoped style and layout component style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
 export default function Home() {
   return (
-    <div>
+    <Layout>
       <h1>Travis build finally passed</h1>
       <Link to="/about">About</Link>
-    </div>
+    </Layout>
   )
 }

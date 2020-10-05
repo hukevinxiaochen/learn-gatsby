2dc2d59ac40702ae2e8dfbb2a4f488cc02eed256
Kevin Hu <hukxchen+git@gmail.com>
2020-10-05 08:29:10 -0500

    feat: note renders actual content
    from the markdown file! uses graphQL query to pull this

--- src/templates/note.js
+++ src/templates/note.js
@@ -1,10 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function Note() {
+export default function Note({ data }) {
+  const note = data.markdownRemark
   return (
     <Layout>
-      <h1>Note</h1>
+      <h1>{note.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: note.html }}></div>
+      <p>A note rendered from markdown</p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
